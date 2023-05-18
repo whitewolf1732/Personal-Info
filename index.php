30ded8d8fe9307cc45ce10283449a1386840eaff
whitewolf1732 <133659354+whitewolf1732@users.noreply.github.com>
2023-05-18 20:07:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+            <title>Personal Information</title>
+            <style type="text/css">
+                body{
+                   font-style: italic;
+                   padding-left: 500px;
+                    padding-right: 500px;
+                    padding-top: 50px;
+                    background-color: black;
+                    color: white;
+                    
+                }
+                form{
+                    border-style:double ;
+                    border-color: white;
+                    padding-left: 0px;
+                    padding-right: 0px;
+                    
+                }
+                p.a{
+                    border-style:dashed;
+                    border-color: whitesmoke;
+                    
+                }
+            </style>
+    </head>
+    <body>
+        <form >
+            <h1 align=center><u>Resume</u></h1>
+            <p><b>Name:</b>Abishek M</p>
+<p ><b>Age:</b>24</p>
+<p ><b>Sex:</b>Male</p>
+<p ><b>Address:</b>No.10, New Street,Iyan Kulam,Vellore-10</p>
+<p ><b>Qualification:</b></p>
+<table border="1">
+    <th>
+     Education 
+    </th><th>
+        Year
+    </th>
+    <tr>
+        <td>HSC</td><td>2016-2017</td></tr>
+    <tr><td>B.Tech(IT)</td><td>2017-2021</td></tr>
+</table>   <br>
+        
+
+ <b > Skills:</b>
+ <ol>
+            <li>Java</li>
+            <li>Python</li>
+            <li>HTML</li>
+            <li>PhP</li>
+            <li>MySQL</li>
+            <li>C</li>
+        </ol>
+       
+
+ </form>
+        <P class="a" align="center" >Note:Please click on the Weekday to move to the Timetable
+            <table border="1" align="center">
+                <tr><td><a href="monday.html" >Monday</a></td>
+                    <td><a href="Tuesday.html" >Tuesday</a></td></tr><tr>
+                    <td><a href="Wednesday.html">Wednesday</a></td>
+                    <td><a href="Thursday.html">Thursday</a></td></tr><tr>
+                    <td><a href="Friday.html">Friday</a></td>
+                    <td><a href="Saturday.html">Saturday</a></td></tr><tr>
+                    <td><a href="Sunday.html">Sunday</a></td></tr></table>
+           
+    </p>
+                
+    </body>
+</html>
